8ac9fa9c52478941084266ec11e1d45b23695ce1
Atharva Wagh <gameboy369065@gmail.com>
2021-11-10 18:36:18 +0530

    Add files via upload

---
+++ components/Shoot.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent("bullets",{
+    init : function(){
+        this.shootBullet();
+        },
+    shootBullet: function(){
+        window.addEventListener("keydown",(e)=>{
+            if(e.key === "b"){
+                var bullet = document.createElement("a-entity");
+                bullet.setAttribute("geometry",{
+                    "primitive":"sphere",
+                    "radius":"0.1"
+                });
+                bullet.setAttribute("material",{
+                    "color":"black"
+                });
+                var camera = document.querySelector("#camera").object3D;
+                var cam = document.querySelector("#camera");
+                var direction = new THREE.Vector3();
+                camera.getWorldDirection(direction);
+                pos = cam.getAttribute("position");
+                bullet.setAttribute("position",{
+                    "x":pos.x,
+                    "y":pos.y,
+                    "z":pos.z
+                });
+                bullet.setAttribute("velocity",direction.multiplyScalar(-10))
+                var scene = document.querySelector("#scene");
+                scene.appendChild(bullet);
+            }
+        })
+    }    
+})
